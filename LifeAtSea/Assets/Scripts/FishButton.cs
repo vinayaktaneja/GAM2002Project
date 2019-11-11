01e9669ddb2434d34bf0e75b8226888723a71939
Vinayak Taneja <VT238@live.mdx.ac.uk>
2019-11-11 17:33:50 +0000

    First update
    Added background and change scene button

---
+++ LifeAtSea/Assets/Scripts/FishButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FishButton : MonoBehaviour
+{
+
+    public void fishSceneChange(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
