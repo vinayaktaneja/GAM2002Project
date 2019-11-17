063c05bc65becc0cbd13da01f87dc4db3d706fc7
Vinayak Taneja <VT238@live.mdx.ac.uk>
2019-11-17 03:31:27 +0000

    Second update (15/11/2019)
    Added and scripted fishing hook (Left, right, cast and recall)

---
+++ LifeAtSea/Assets/Scripts/HookMovement.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookMovement : MonoBehaviour
+{
+
+    public float min_z = -55f, max_z = 55f;
+    public float rotate_Speed = 5f;
+
+    private float rotate_Angle;
+    private bool rotate_Right;
+    private bool canRotate;
+
+    public float move_Speed = 3f;
+    private float initial_Move_Speed;
+
+    public float min_Y = -2.5f;
+    private float initial_Y;
+
+    private bool moveDown;
+
+    //For line renderer
+    // private RopeRenderer ropeRenderer;
+
+
+
+  
+    void Start()
+    {
+        initial_Y = transform.position.y;
+        initial_Move_Speed = move_Speed;
+
+        canRotate = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+        GetInput();
+        MoveRope();
+    }
+
+    void Rotate()
+    {
+        if (!canRotate)
+            return;
+
+        if (rotate_Right)
+        {
+            rotate_Angle += rotate_Speed * Time.deltaTime;
+        }
+        else
+        {
+            rotate_Angle -= rotate_Speed * Time.deltaTime;
+        }
+
+        transform.rotation = Quaternion.AngleAxis(rotate_Angle, Vector3.forward);
+
+        if (rotate_Angle >= max_z)
+        {
+            rotate_Right = false;
+        }
+        else if (rotate_Angle <= min_z)
+        {
+            rotate_Right = true;
+        }
+    }
+
+    void GetInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (canRotate)
+            {
+                canRotate = false;
+                moveDown = true;
+            }
+        }
+    }
+
+    void MoveRope()
+    {
+        if (canRotate)
+            return;
+
+        if (!canRotate)
+        {
+            Vector3 temp = transform.position;
+
+            if (moveDown)
+            {
+                temp -= transform.up * Time.deltaTime * move_Speed;
+            }
+            else
+            {
+                temp += transform.up * Time.deltaTime * move_Speed;
+            }
+
+            transform.position = temp;
+
+            if(temp.y <= min_Y)
+            {
+                moveDown = false;
+            }
+            if (temp.y >= initial_Y)
+            {
+                canRotate = true;
+                move_Speed = initial_Move_Speed;
+            }
+        }
+    }
+}
